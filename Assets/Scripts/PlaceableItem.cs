f83b649ecff7016b412a87f4e380aa403c21f689
Arkos <arkos.game.lab@gmail.com>
2025-04-17 04:47:19 +0500

    - carry items

--- Assets/Scripts/PlaceableItem.cs
+++ Assets/Scripts/PlaceableItem.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlaceableItem : MonoBehaviour
 {
@@ -11,6 +12,9 @@ public class PlaceableItem : MonoBehaviour
     }
 
 
+    [Header(nameof(Collider))]
+    [SerializeField] private Collider _collider;
+
     [Header(nameof(Material))]
     [SerializeField] private Renderer _renderer;
     [SerializeField] private Material _commonMaterial;
@@ -19,11 +23,19 @@ public class PlaceableItem : MonoBehaviour
     [Header(nameof(StackView))]
     [SerializeField] private StackView _stack;
 
+    [Header("Model")]
+    [SerializeField] private Transform _model;
+
 
     private State _state = State.Undefined;
+    private Vector3 _positionCache;
+    private Quaternion _rotationCache;
 
 
-    public bool Pick()
+    public Vector3 ModelScale => _model.lossyScale;
+
+
+    public bool TryPick()
     {
         switch (_state)
         {
@@ -31,10 +43,43 @@ public class PlaceableItem : MonoBehaviour
 
             case State.Placed:
                 _renderer.material = _pickedMaterial;
+                _renderer.shadowCastingMode = ShadowCastingMode.Off;
+                _collider.enabled = false;
+
+                var tf = transform;
+                _positionCache = tf.position;
+                _rotationCache = tf.rotation;
+
                 _state = State.Picked;
                 return true;
 
             case State.Picked:
+                return false;
+
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    public void MoveTo(Vector3 position, Quaternion rotation)
+    {
+        var tf = transform;
+        tf.position = position;
+        tf.rotation = rotation;
+    }
+
+    public bool TryPlace(Vector3? position = null, Quaternion? rotation = null)
+    {
+        switch (_state)
+        {
+            case State.Undefined:
+                throw new NotSupportedException(nameof(State.Undefined));
+
+            case State.Picked:
+                Place(position ?? _positionCache, rotation ?? _rotationCache);
+                return true;
+
+            case State.Placed:
                 return false;
 
             default:
@@ -47,10 +92,10 @@ public class PlaceableItem : MonoBehaviour
         switch (_state)
         {
             case State.Undefined:
+                throw new NotSupportedException(nameof(State.Undefined));
 
             case State.Picked:
-                _renderer.material = _commonMaterial;
-                _state = State.Placed;
+                Place(_positionCache, _rotationCache);
                 return true;
 
             case State.Placed:
@@ -59,5 +104,17 @@ public class PlaceableItem : MonoBehaviour
             default:
                 throw new ArgumentOutOfRangeException();
         }
+    }
+
+
+    private void Place(Vector3 position, Quaternion rotation)
+    {
+        _renderer.material = _commonMaterial;
+        _renderer.shadowCastingMode = ShadowCastingMode.Off;
+        _collider.enabled = true;
+
+        MoveTo(position, rotation);
+
+        _state = State.Placed;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -35,18 +35,19 @@ public class PlayerController : MonoBehaviour
         if (Time.time < 0.1f)
             return;
 
-        CameraControls();
-        Movement();
-        HandleItems();
+        var tf = transform;
+        CameraControls(tf);
+        Movement(tf);
+        HandleItems(tf);
     }
 
 
-    private void CameraControls()
+    private void CameraControls(Transform tf)
     {
         // TODO: use new input system or Rewired etc
         var mousePosition = Input.mousePositionDelta;
 
-        var playerEuler = transform.localEulerAngles;
+        var playerEuler = tf.localEulerAngles;
         var cameraEuler = _camera.localEulerAngles;
         if (cameraEuler.x > 180f)
             cameraEuler.x -= 360f;
@@ -54,11 +55,11 @@ public class PlayerController : MonoBehaviour
         playerEuler.y += mousePosition.x;
         cameraEuler.x = Mathf.Clamp(cameraEuler.x - mousePosition.y, -_cameraXMaxUp, _cameraXMaxDown);
 
-        transform.localEulerAngles = playerEuler;
+        tf.localEulerAngles = playerEuler;
         _camera.localEulerAngles = cameraEuler;
     }
 
-    private void Movement()
+    private void Movement(Transform tf)
     {
         // TODO: use new input system or Rewired etc
         var input = Vector3.zero;
@@ -72,45 +73,58 @@ public class PlayerController : MonoBehaviour
             input.x++;
         var speed = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift) ? _sprint : _speed;
 
-        var tf = transform;
         var desiredDistance = Time.deltaTime * speed;
         var desiredLocalMove = input * desiredDistance;
         var desiredMove = tf.forward * desiredLocalMove.z + tf.right * desiredLocalMove.x;
         tf.position += desiredMove;
     }
 
-    private void HandleItems()
+    private void HandleItems(Transform tf)
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
+        {
+            var item = DetectItem(tf);
+            if (item)
+            {
+                if (_currentItem)
+                {
+                    _currentItem.Unpick();
+                    _currentItem = null;
+                }
+
+                _currentItem = item;
+                _currentItem.TryPick();
+            }
+            else
+            {
+                if (_currentItem)
+                {
+                    _currentItem.TryPlace();
+                    _currentItem = null;
+                }
+            }
+        }
+        else
+        if (Input.GetMouseButtonDown(1))
         {
             if (_currentItem)
             {
                 _currentItem.Unpick();
                 _currentItem = null;
             }
-
-            var item = DetectItem();
-            if (!item)
-                return;
-
-            _currentItem = item;
-            _currentItem.Pick();
         }
         else
-        if (Input.GetMouseButton(1))
         {
             if (_currentItem)
             {
-                _currentItem.Unpick();
-                _currentItem = null;
+                var itemScale = _currentItem.ModelScale;
+                _currentItem.MoveTo(tf.position + tf.forward * (1 + itemScale.z / 2f), tf.rotation);
             }
         }
     }
 
-    private PlaceableItem DetectItem()
+    private PlaceableItem DetectItem(Transform tf)
     {
-        var tf = _camera.transform;
-
         // TODO: non-alloc
         if (!Physics.Raycast(tf.position, tf.forward, out var hit, _pickDistance, _itemsMask))
             return null;
